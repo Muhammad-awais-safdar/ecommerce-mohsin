ce23c71bffac097336e6a1c25a9099910b55d904
awais <awaissafdar111@gmail.com>
2025-04-19 23:11:00 +0500

    added

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use App\Http\Controllers\OrderItemController;
 Route::get('/', [FrontendController::class, 'index'])->name('home');
 Route::get('/product/{id}', [FrontendController::class, 'show'])->name('product.show');
 
-// Admin routes
-Route::get('/admin/products', [ProductController::class, 'admin'])->name('admin.products');
-Route::get('/admin/products/create', [ProductController::class, 'create'])->name('admin.products.create');
-Route::post('/admin/products', [ProductController::class, 'store'])->name('admin.products.store');
+// // Admin routes
+// Route::get('/admin/products', [ProductController::class, 'admin'])->name('admin.products');
+// Route::get('/admin/products/create', [ProductController::class, 'create'])->name('admin.products.create');
+// Route::post('/admin/products', [ProductController::class, 'store'])->name('admin.products.store');
 
 
 
@@ -26,20 +26,20 @@ Route::post('/admin/products', [ProductController::class, 'store'])->name('admin
 
 
 
-// Cart Routes
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/add/{productId}', [CartController::class, 'add'])->name('cart.add');
-Route::post('/cart/remove/{cartItemId}', [CartController::class, 'remove'])->name('cart.remove');
+// // Cart Routes
+// Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+// Route::post('/cart/add/{productId}', [CartController::class, 'add'])->name('cart.add');
+// Route::post('/cart/remove/{cartItemId}', [CartController::class, 'remove'])->name('cart.remove');
 
-// CartItem Routes
-Route::post('/cart-item/update/{cartItemId}', [CartItemController::class, 'update'])->name('cart-item.update');
+// // CartItem Routes
+// Route::post('/cart-item/update/{cartItemId}', [CartItemController::class, 'update'])->name('cart-item.update');
 
-// Order Routes
-Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout.form');
-Route::post('/checkout', [OrderController::class, 'process'])->name('checkout.process');
+// // Order Routes
+// Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout.form');
+// Route::post('/checkout', [OrderController::class, 'process'])->name('checkout.process');
 
-// OrderItem Routes (Optional, for viewing)
-Route::get('/order/{orderId}', [OrderItemController::class, 'show'])->name('order.show');
+// // OrderItem Routes (Optional, for viewing)
+// Route::get('/order/{orderId}', [OrderItemController::class, 'show'])->name('order.show');
 
 
 
