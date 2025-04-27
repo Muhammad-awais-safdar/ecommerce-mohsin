bb7ba2fe138878250daa79d9b0fefa3ee51a7b95
awais <awaissafdar111@gmail.com>
2025-04-27 12:30:38 +0500

    pagination added

--- resources/views/Ecommerce/pages/shop.blade.php
+++ resources/views/Ecommerce/pages/shop.blade.php
@@ -26,62 +26,62 @@
 
                         <ul class="row list-products auto-clear equal-container product-grid">
                             @foreach ($products as $product)
-                                                    <li class="product-item  col-lg-3 col-md-4 col-sm-6 col-xs-6 col-ts-12 style-1">
-                                                        <div class="product-inner equal-element">
-                                                            <div class="product-top">
-                                                                <div class="flash">
-                                                                    <span class="onnew">
-                                                                        <span class="text">
-                                                                            new
-                                                                        </span>
-                                                                    </span>
-                                                                </div>
-                                                            </div>
-                                                            <div class="product-thumb">
-                                                                <div class="thumb-inner">
-                                                                    <a href="#">
-                                                                        <img src="{{ asset($product->image) }}" alt="img">
-                                                                    </a>
-                                                                    <div class="thumb-group">
+                                <li class="product-item  col-lg-3 col-md-4 col-sm-6 col-xs-6 col-ts-12 style-1">
+                                    <div class="product-inner equal-element">
+                                        <div class="product-top">
+                                            <div class="flash">
+                                                <span class="onnew">
+                                                    <span class="text">
+                                                        new
+                                                    </span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="product-thumb">
+                                            <div class="thumb-inner">
+                                                <a href="#">
+                                                    <img src="{{ asset($product->image) }}" alt="img">
+                                                </a>
+                                                <div class="thumb-group">
 
-                                                                        <div class="loop-form-add-to-cart">
-                                                                            <button class="single_add_to_cart_button button"
-                                                                                onclick="addToCart({{ $product->id }})"">Add to cart
-                                                                                    </button>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class=" product-info">
-                                                                                <h5 class="product-name product_title">
-                                                                                    <a href="#">{{ $product->name }}</a>
-                                                                                </h5>
-                                                                                <div class="group-info">
-                                                                                    <div class="stars-rating">
-                                                                                        <div class="star-rating">
-                                                                                            <span class="star-3"></span>
-                                                                                        </div>
-                                                                                        <div class="count-star">
-                                                                                            (3)
-                                                                                        </div>
-                                                                                    </div>
-                                                                                    <div class="price">
-                                                                                        <del>
-                                                                                            @php
-    $price = $product->price;
-    $price = number_format($price, 2);
-    $unorderedprice = $price + rand(10, 100);
-                                                                                            @endphp
-                                                                                            ${{ $unorderedprice }}
-                                                                                        </del>
-                                                                                        <ins>
-                                                                                            ${{ $price }}
-                                                                                        </ins>
-                                                                                    </div>
-                                                                                </div>
-                                                                        </div>
-                                                                    </div>
-                                                    </li>
+                                                    <div class="loop-form-add-to-cart">
+                                                        <button class="single_add_to_cart_button button"
+                                                            onclick="addToCart({{ $product->id }})"">Add to cart
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class=" product-info">
+                                            <h5 class="product-name product_title">
+                                                <a href="#">{{ $product->name }}</a>
+                                            </h5>
+                                            <div class="group-info">
+                                                <div class="stars-rating">
+                                                    <div class="star-rating">
+                                                        <span class="star-3"></span>
+                                                    </div>
+                                                    <div class="count-star">
+                                                        (3)
+                                                    </div>
+                                                </div>
+                                                <div class="price">
+                                                    <del>
+                                                        @php
+                                                            $price = $product->price;
+                                                            $price = number_format($price, 2);
+                                                            $unorderedprice = $price + rand(10, 100);
+                                                        @endphp
+                                                        ${{ $unorderedprice }}
+                                                    </del>
+                                                    <ins>
+                                                        ${{ $price }}
+                                                    </ins>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
                             @endforeach
 
 
@@ -105,7 +105,7 @@
                 data: {
                     _token: '{{ csrf_token() }}'
                 },
-                success: function (response) {
+                success: function(response) {
                     if (response.status === 'success') {
                         // ✅ Update cart count
                         $('#cart-count').text(response.cart_count);
