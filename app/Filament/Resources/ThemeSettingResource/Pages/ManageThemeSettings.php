5450bbec4ba53e4da21c974f3e1c331d6e74e532
awais <awaissafdar111@gmail.com>
2025-05-07 22:56:27 +0500

    changes

--- app/Filament/Resources/ThemeSettingResource/Pages/ManageThemeSettings.php
+++ app/Filament/Resources/ThemeSettingResource/Pages/ManageThemeSettings.php
@@ -1,8 +1,11 @@
-<?php 
+<?php
 
 namespace App\Filament\Resources\ThemeSettingResource\Pages;
 
 use App\Filament\Resources\ThemeSettingResource;
+use Filament\Actions\Action;
+use App\Models\ThemeSetting;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageThemeSettings extends ManageRecords
@@ -11,7 +14,34 @@ class ManageThemeSettings extends ManageRecords
 
     protected function getHeaderActions(): array
     {
-        return []; // Disable "Create" button
+        return [
+            Action::make('reset_colors')
+                ->label('Reset Colors')
+                ->color('danger')
+                ->requiresConfirmation()
+                ->action(function () {
+                    $defaultColors = [
+                        'color_background' => '#ffffff',
+                        'color_text_primary' => '#2C2C2C',
+                        'color_text_secondary' => '#8C8C8C',
+                        'color_accent_primary' => '#C7A200',
+                        'color_accent_secondary' => '#FFD700',
+                        'color_border' => '#DDD6C5',
+                    ];
+
+                    foreach ($defaultColors as $key => $value) {
+                        ThemeSetting::updateOrCreate(
+                            ['key' => $key],
+                            ['value' => $value]
+                        );
+                    }
+
+                    Notification::make()
+                        ->title('Theme colors have been reset to default.')
+                        ->success()
+                        ->send();
+                }),
+        ];
     }
 
     public function isTableSearchable(): bool
