d039dd4c4fa82c848a50b4b152d86cf069685230
awais <awaissafdar111@gmail.com>
2025-05-15 22:39:40 +0500

    changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Providers;
+
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Artisan;
 use App\Services\SeoService;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,9 +22,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            Artisan::starting(function ($artisan) {
+                $command = $artisan->output();
+
+                // Block migrate:fresh and migrate:refresh
+                $forbiddenCommands = ['migrate:fresh', 'migrate:refresh'];
+
+                if (in_array($artisan->getName(), $forbiddenCommands)) {
+                    exit("The {$artisan->getName()} command is disabled in production environment.\n");
+                }
+            });
+        }
         View::composer('*', function ($view) {
-        $seo = app(SeoService::class)->getSeoData();
-        $view->with('seo', $seo);
-    });
+            $seo = app(SeoService::class)->getSeoData();
+            $view->with('seo', $seo);
+        });
     }
 }
